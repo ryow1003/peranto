01cdbb1970e7f8329431e0d328e4d490a15eb7e2
Ryow <64392776+ryow1003@users.noreply.github.com>
2021-02-25 01:10:46 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -3,9 +3,27 @@ window.onload = function() {
     $('.load').fadeOut(1000);
   }, 1000);
 }
-
+$(function () {
+  var $body = $('body');
+  $('#btn-menu').on('click', function () {
+    $body.toggleClass('open-menu');
+  });
+  $('#sp-menu').on('click', function () {
+    $body.removeClass('open-menu');
+  });
+});
 $(function(){
   $('a[href^="#second"]').click(function(){
+    var speed = 800;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+});
+$(function(){
+  $('a[href^="#top-jump"]').click(function(){
     var speed = 800;
     var href= $(this).attr("href");
     var target = $(href == "#" || href == "" ? 'html' : href);
